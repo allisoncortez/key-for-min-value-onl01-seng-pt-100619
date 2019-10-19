aee807fc5d4b27b2d8050e721f149fe9b0e8e587
allison cortez <allizetorc@gmail.com>
2019-10-19 01:51:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,12 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   
   name_hash.each do |key, value|
-    if lowest_value == nil 
+    if lowest_value == nil || value < lowest_value
       lowest_key = key 
       lowest_value = value
-    elsif value < lowest_value
-      lowest_key = key 
-      lowest_value = value 
+    # elsif value < lowest_value
+    #   lowest_key = key 
+    #   lowest_value = value 
     end
   end
   lowest_key
@@ -27,20 +27,4 @@ end
 #     end
 #   end
 #   lowest_key
-# end
-
-# def key_for_min_value(name_hash)
-#   if name_hash = {}
-#     return nil
-#   else
-#       min_value = name_hash.first[1]
-#       min_key = name_hash.first[0]
-#       name_hash.each do |key, value|
-#         if value < min_value
-#           min_value = value
-#           min_key = key
-#         end
-#       end
-#       min_key
-#   end 
 # end
